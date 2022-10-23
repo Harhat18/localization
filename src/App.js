613a69282ade48d89e292dad0f4440743724964c
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-10-23 17:08:59 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import { useState } from "react";
+import { IntlProvider, FormattedMessage, FormattedNumber } from "react-intl";
+const messages = {
+  "tr-TR": {
+    title: "Merhaba Dünya",
+    description: "3 yeni mesajınız var",
+  },
+  "en-US": {
+    title: "Hello World",
+    description: "you have 3 new messages",
+  },
+};
+
+function App() {
+  const [lang, setLang] = useState("tr-TR");
+  return (
+    <div className="App">
+      <IntlProvider messages={messages[lang]}>
+        <FormattedMessage id="title" />
+        <br></br>
+        <br></br>
+        <button onClick={() => setLang("tr-TR")}>TR</button>
+        <button onClick={() => setLang("en-US")}>EN</button>
+      </IntlProvider>
+    </div>
+  );
+}
+
+export default App;
